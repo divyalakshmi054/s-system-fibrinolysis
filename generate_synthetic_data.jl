8cdca98414f4a9a9eb791fdd051a2f97397dca91
divyalakshmi054 <divyalakshmi054@gmail.com>
2023-03-11 08:36:33 -0500

    more changes to synthetic file; adding Training-Composition.CSV to box

--- generate_synthetic_data.jl
+++ generate_synthetic_data.jl
@@ -19,14 +19,14 @@ data_col_names_tuple = Tuple(Symbol.(names(full_df)[3:end]))
 number_of_fields = length(data_col_symbols)
 
 # what visit are we looking at?
-# visit_df = filter(:visitid => x->(x==2 || x==3), full_df)
+visit_df = filter(:Visit => x->(x==4), full_df) #let's filter visit 4s since we look to train using that visit
 
 # sample -
 number_of_synthetic_samples = 1000
 data_array = Matrix(full_df[!, 3:end])
 μ = mean(data_array, dims = 1)
 Σ = cov(data_array)
-D = MvNormal(reshape(μ,14), Σ)
+D = MvNormal(reshape(μ,18), Σ)
 synthetic_sampled_data = rand(D, number_of_synthetic_samples)
 
 # create a new data frame -
@@ -54,11 +54,14 @@ scale_factor_dict[:XI] = (1e-8)*SF
 scale_factor_dict[:XII] = (1e-8)*SF
 scale_factor_dict[:AT] = (3.4e-6)*SF
 scale_factor_dict[:PC] = (6.3e-8)*SF
-scale_factor_dict[:TFPI] = (2.9e-11)*SF
-scale_factor_dict[:Fbgn] = (2.9e-8)*SF
-scale_factor_dict[:Plgn] = (1.09e-8)*SF
-scale_factor_dict[:TAFI] = 75.0 #re-check
-#scale_factor_dict[:PAI1] = 1.0
+scale_factor_dict[:TFPI] = (2.9e-11)*SF #calculated using molecular mass of compound
+scale_factor_dict[:Fbgn] = (2.9e-8)*SF  #calculated using molecular mass of compound
+scale_factor_dict[:Plgn] = (1.09e-8)*SF #calculated using molecular mass of compound
+scale_factor_dict[:TAFI] = 75.0
+scale_factor_dict[:a2AP] = (1.53e-8)*SF #calculated using molecular mass of compound - let's revisit this later
+scale_factor_dict[:RBC] = 1.0 #let's leave these alone for now
+scale_factor_dict[:PLT] = 1.0 #let's leave these alone for now
+scale_factor_dict[:FXIII] = 68.0
 
 
 # ok, finally - let's convert these data into the correct units - TFPI & PAI1 is already in nM, the rest 
@@ -69,7 +72,7 @@ for i ∈ 1:number_of_synthetic_samples
         
         # look up the scale factor -
         fld_symbol = data_col_symbols[j]
-        if(fld_symbol == :TFPI || fld_symbol == :Plgn || fld_symbol == :Fbgn)
+        if(fld_symbol == :TFPI || fld_symbol == :Plgn || fld_symbol == :Fbgn || fld_symbol == :a2AP || fld_symbol == :RBC || fld_symbol == :PLT)
             SF_val = scale_factor_dict[fld_symbol]
             synthetic_data_frame[i,fld_symbol] = SF_val*synthetic_data_frame[i,fld_symbol]
         elseif(haskey(scale_factor_dict,fld_symbol) == true)
